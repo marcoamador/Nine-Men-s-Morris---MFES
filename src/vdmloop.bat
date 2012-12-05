4cf18d43f9a9e43f4d7bf0c9b46d1b553925986d
Marco Amador <marco.amador91@gmail.com>
2012-12-05 01:39:37 +0000

    TestPlayer e Player com test coverage a 100%

--- src/vdmloop.bat
+++ src/vdmloop.bat
@@ -2,13 +2,12 @@
 rem Runs a collection of VDM++ test examples
 rem Assumes specification is in Word RTF files
 
-set S1=Board.rtf
-set S2=TestBoard.rtf
-set S3=Game.rtf
-set S4=TestGame.rtf
-set S5=Player.rtf
-set S6=TestPlayer.rtf
+set S1=Player.rtf
+set S2=TestPlayer.rtf
+set S3=Board.rtf
+set S4=TestBoard.rtf
+set S5=Game.rtf
+set S6=TestGame.rtf
 
-
-"D:\VDM++Toolbox_v8.1.1b\bin\vppde" -p -R vdm.tc %S1% %S2%
+"D:\VDM++Toolbox_v8.1.1b\bin\vppde" -p -R vdm.tc %S1% %S2% %S3% %S4% %S5% %S6%
 for /R %%f in (*.arg) do call vdmtest "%%f"
--- src/vdmtest.bat
+++ src/vdmtest.bat
@@ -1,19 +1,21 @@
 @echo off
+
 rem Tests the date book specification for one test case (argument)
 rem -- Output the argument to stdout (for redirect) and "con" (for user feedback)
 echo VDM Test: '%1' > con
 echo VDM Test: '%1'
 
 rem short names for specification files in Word RTF Format
-set S1=Board.rtf
-set S2=TestBoard.rtf
-set S3=Game.rtf
-set S4=TestGame.rtf
-set S5=Player.rtf
-set S6=TestPlayer.rtf
+set S1=Player.rtf
+set S2=TestPlayer.rtf
+set S3=Board.rtf
+set S4=TestBoard.rtf
+set S5=Game.rtf
+set S6=TestGame.rtf
+
 
 rem -- Calls the interpreter for this test case
-"D:\VDM++Toolbox_v8.1.1b\bin\vppde" -i -D -I -P -Q -R vdm.tc -O %1.res %1 %S1% %S2%
+"D:\VDM++Toolbox_v8.1.1b\bin\vppde" -i -D -I -P -Q -R vdm.tc -O %1.res %1 %S1% %S2% %S3% %S4% %S5% %S6%
 
 rem -- Check for difference between result of execution and expected result.
 if EXIST %1.exp fc /w %1.res %1.exp
